99ac7d0fab6249b27b041bd88ce0bf4d5b242798
Kevin Morris <morrisk@brennan.law.nyu.edu>
2019-05-01 21:14:07 -0400

    add code for NYS

--- code/florida/02_genmatch_fl.R
+++ code/florida/02_genmatch_fl.R
@@ -16,20 +16,20 @@ NodeFile = Sys.getenv("MY_HOSTFILE")
 
 cl <- makeCluster(c(readLines(NodeFile)), type="SOCK")
 for(i in 1:10){
-  fl_sample <- readRDS("./temp/florida_race_census.RDS") %>%
+  ny_sample <- readRDS("./temp/new_york_race_census.RDS") %>%
     group_by(uncontested) %>%
     sample_frac(0.01) %>%
     ungroup() %>% 
-    dplyr::select(uncontested, dem, rep, yob, gender, white, black, latino.x, voted_primary, median_income, some_college)
+    dplyr::select(uncontested, dem, rep, yob, gender, pred.whi, pred.bla, pred.his, voted_primary, median_income, some_college)
 
 
-  match_data <- fl_sample %>%
-    dplyr::select(dem, rep, yob, gender, white, black, latino.x, voted_primary, median_income, some_college)
+  match_data <- ny_sample %>%
+    dplyr::select(uncontested, dem, rep, yob, gender, pred.whi, pred.bla, pred.his, voted_primary, median_income, some_college)
             
 
-  genout <- GenMatch(Tr = fl_sample$uncontested, X = match_data, pop.size = 1000, cluster = cl)
+  genout <- GenMatch(Tr = ny_sample$uncontested, X = match_data, pop.size = 1000, cluster = cl)
 
-  save(genout, file = paste0("./temp/fl_genmatch_", i, ".Rdata"))
+  save(genout, file = paste0("./temp/ny_genmatch_", i, ".Rdata"))
 }
 
 
