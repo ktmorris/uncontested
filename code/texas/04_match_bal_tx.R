c3b79bc83f88030d3d8de6caaef4e8e79e46f562
Kevin <kevin.morris@nyu.edu>
2019-05-22 09:11:52 -0400

    manually construct balance tables to get around memory constraints

---
+++ code/texas/04_match_bal_tx.R
@@ -0,0 +1,118 @@
+library(Matching)
+library(scales)
+library(kableExtra)
+library(tidyverse)
+library(data.table)
+
+order <- fread("./raw_data/var_orders_ny.csv")
+
+tx <- readRDS("./temp/texas_race_census.RDS") %>% 
+  rename(gender = GENDER)
+
+load("./temp/mout_tx_1.RData")
+
+matches1 <- data.frame("id" = mout[["index.control"]],
+                       "weight" = mout[["weights"]]) %>% 
+  group_by(id) %>% 
+  summarize(weight = sum(weight)) %>% 
+  mutate(treat = F)
+
+matches2 <- data.frame("id" = mout[["index.treated"]],
+                       "weight" = mout[["weights"]]) %>% 
+  group_by(id) %>% 
+  summarize(weight = sum(weight)) %>% 
+  mutate(treat = T)
+
+matches <- bind_rows(matches1, matches2)
+
+tx <- tx %>% 
+  mutate(id = row_number())
+
+matches <- left_join(matches, tx, by = "id")
+
+##########
+means_prematch <- tx %>% 
+  group_by(uncontested) %>% 
+  summarize_at(vars(gender, voted_primary, dem, rep, yob, pred.whi, pred.bla, pred.his, median_income, some_college), funs(mean(.)))
+
+means_postmatch <- matches %>% 
+  group_by(treat) %>% 
+  summarize_at(vars(gender, voted_primary, dem, rep, yob, pred.whi, pred.bla, pred.his, median_income, some_college), funs(sum(weight * .) / sum(weight)))
+
+rm(matches, matches1, matches2)
+
+qqs_post <- lapply(c("gender", "voted_primary", "dem", "rep", "yob", "pred.whi", "pred.bla", "pred.his", "median_income", "some_college"), function(var){
+  j <- select(tx, var)
+  colnames(j) <- c("t")
+  
+  qqout  <- qqstats(j$t[mout$index.treated], j$t[mout$index.control])
+  return(qqout)
+  })
+
+qqs_pre <- lapply(c("gender", "voted_primary", "dem", "rep", "yob", "pred.whi", "pred.bla", "pred.his", "median_income", "some_college"), function(var){
+  j <- select(tx, var, uncontested)
+  colnames(j) <- c("t", "uncontested")
+  
+  qqout  <- qqstats(j$t[j$uncontested == T], j$t[j$uncontested == F])
+  return(qqout)
+})
+
+
+TrMean <- c()
+PreMean <- c()
+PreQQmed <- c()
+PreQQmean <- c()
+PreQQmax <- c()
+PostMean <- c()
+PostQQmed <- c()
+PostQQmean <- c()
+PostQQmax <- c()
+
+i = 1
+for(var in c("gender", "voted_primary", "dem", "rep", "yob", "pred.whi", "pred.bla", "pred.his", "median_income", "some_college")){
+  TrMean <- unlist(c(TrMean, filter(means_prematch, uncontested == T) %>% select(var) %>% pull()))
+  PreMean <- unlist(c(PreMean, filter(means_prematch, uncontested == F) %>% select(var) %>% pull()))
+  
+  PreQQmed <- unlist(c(PreQQmed, qqs_pre[[i]][["mediandiff"]]))
+  PreQQmean <- unlist(c(PreQQmean, qqs_pre[[i]][["meandiff"]]))
+  PreQQmax <- unlist(c(PreQQmax, qqs_pre[[i]][["maxdiff"]]))
+  
+  PostMean <- unlist(c(PostMean, filter(means_postmatch, treat == F) %>% select(var) %>% pull()))
+  PostQQmed <- unlist(c(PostQQmed, qqs_post[[i]][["mediandiff"]]))
+  PostQQmean <- unlist(c(PostQQmean, qqs_post[[i]][["meandiff"]]))
+  PostQQmax <- unlist(c(PostQQmax, qqs_post[[i]][["maxdiff"]]))
+  
+  i = i + 1
+}
+
+
+
+varnames <- c("gender", "voted_primary", "dem", "rep", "yob", "pred.whi", "pred.bla", "pred.his", "median_income", "some_college")
+
+
+df <- data.frame("TrMean" = TrMean,
+                 "TrMean2" = TrMean,
+                 "PreMean" = PreMean,
+                 "PreQQmed" = PreQQmed,
+                 "PreQQmean" = PreQQmean,
+                 "PreQQmax" = PreQQmax,
+                 "PostMean" = PostMean,
+                 "PostQQmed" = PostQQmed,
+                 "PostQQmean" = PostQQmean,
+                 "PostQQmax" = PostQQmax,
+                 "names" = varnames) %>%
+  mutate(change_mean = 1 - (abs(TrMean - PostMean) / abs(TrMean - PreMean)),
+         change_eqqmed = 1 - abs(PostQQmed / PreQQmed),
+         change_eqqmean = 1 - abs(PostQQmean / PreQQmean),
+         change_eqqmax = 1 - abs(PostQQmax / PreQQmax)) %>%
+  mutate_at(vars(TrMean, PreMean, TrMean2, PostMean), funs(comma(round(., 2), accuracy = .01))) %>%
+  mutate_at(vars(change_mean, change_eqqmed, change_eqqmean, change_eqqmax), funs(round(. * 100, 2)))
+
+df <- full_join(df, order, by = c("names" = "variable")) %>%
+  arrange(order) %>%
+  select(name, TrMean, PreMean, TrMean2, PostMean, change_mean, change_eqqmed, change_eqqmean, change_eqqmax) %>%
+  filter(!is.na(TrMean))
+
+colnames(df) <- c("", "Treated", "Control", "Treated", "Control", "Mean Diff", "eQQ Med", "eQQ Mean", "eQQ Max")
+
+saveRDS(df, "./temp/match_table_tx.rds")
