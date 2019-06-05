e912049488d46cb7daf345be6957275158f1968d
Kevin <kevin.morris@nyu.edu>
2019-06-05 15:20:28 -0400

    election results - still v messy

--- code/r_code/wisconsin/01_code_wi.R
+++ code/r_code/wisconsin/01_code_wi.R
@@ -1,6 +1,6 @@
 # 
 # ## read in wisconsin voter file
-# wi <- dbGetQuery(db, "select Voter_Reg_Number, LastName, FirstName, County, Congressional, State_Assembly,
+# wi <- dbGetQuery(db, "select Voter_Reg_Number, LastName, FirstName, County, Congressional, State_Assembly, Jurisdiction, Ward,
 #                       Address1, Address2, November2018, August2018, ApplicationDate, Voter_Status from wi_roll_0319
 #                      where Voter_Status == 'Active'") %>%
 #   filter(as.Date(gsub(" [A-z ]*", "" , ApplicationDate), "%m/%d/%Y") <= "2018-10-17") %>% 
@@ -32,6 +32,77 @@ wi$voted_primary <- wi$August2018 %in% c("Absentee", "At Polls")
 wi$voted_general <- wi$November2018 %in% c("Absentee", "At Polls")
 
 wi <- dplyr::select(wi, -August2018, -November2018)
+
+#### read in 2016 election results for party controls
+
+r16 <- fread("./raw_data/wi_results/cleaned_file_2016_results.csv")
+colnames(r16) <- gsub("[.]", "_", make.unique(make.names(colnames(r16))))
+r16$share_r <- r16$Donald_J__Trump___Michael_R__Pence / r16$Total_Votes
+r16 <- select(r16, County_Name, Municipality_Name, Reporting_Unit, share_r)
+# start with non-consequtive precincts - they have commas
+
+nonc <- r16 %>% 
+  filter(grepl(",", Reporting_Unit))
+nonc <- cSplit(nonc, "Reporting_Unit", sep = ",", type.convert = F, drop = F, direction = "long")
+
+nonc$ward <- gsub(".*Wards ", "", nonc$Reporting_Unit)
+nonc$ward <- gsub(".*Ward ", "", nonc$ward)
+nonc <- nonc[!is.na(nonc$ward), ]
+
+nonc_cool <- nonc[!grepl("-", nonc$ward)] %>% 
+  select(-Reporting_Unit, -Reporting_Unit)
+
+nonc_more <- nonc %>% 
+  filter(grepl("-", ward)) %>% 
+  mutate(ward = gsub("A", "", ward))
+nonc_more <- cSplit(nonc_more, "ward", sep = "-", type.convert = F)
+for(i in 1:nrow(nonc_more)){
+  nonc_more$ward[i] <- paste(c(nonc_more$ward_1[i]:nonc_more$ward_2[i]), collapse = ",")
+}
+nonc_more <- select(nonc_more, -ward_1, -ward_2, -Reporting_Unit)
+nonc_more <- cSplit(nonc_more, "ward", sep = ",", direction = "long", type.convert = F)
+
+nonc <- bind_rows(nonc_cool, nonc_more)
+# now consequtive precincts
+
+others <- r16 %>% 
+  mutate(Reporting_Unit = gsub("Wards", "Ward", Reporting_Unit)) %>% 
+  filter(grepl("-", gsub(".*Ward ", "", Reporting_Unit)),
+         !grepl(",", Reporting_Unit))
+
+others$ward <- gsub(".*Wards ", "", others$Reporting_Unit)
+others$ward <- gsub(".*Ward ", "", others$ward)
+others$ward <- gsub("B", "", others$ward)
+others$ward <- gsub("A", "", others$ward)
+others <- cSplit(others, "ward", sep = "-", type.convert = F)
+
+for(i in 1:nrow(others)){
+  others$ward[i] <- paste(c(others$ward_1[i]:others$ward_2[i]), collapse = ",")
+}
+others <- cSplit(others, "ward", sep = ",", direction = "long", type.convert = F)
+others$ward <- ifelse(others$Reporting_Unit == "TOWN OF LA GRANGE Ward 1B-3B", paste0(others$ward, "00"), others$ward)
+others <- select(others, -Reporting_Unit, - ward_1, -ward_2)
+
+# fine ones
+fine <- r16 %>% 
+  mutate(Reporting_Unit = gsub("Wards", "Ward", Reporting_Unit)) %>% 
+  filter(!grepl("-", gsub(".*Ward ", "", Reporting_Unit)),
+         !grepl(",", Reporting_Unit)) %>% 
+  mutate(ward = gsub(".*Ward ", "", Reporting_Unit)) %>% 
+  select(-Reporting_Unit)
+##
+r16 <- bind_rows(fine, others, nonc_more, nonc_cool) %>% 
+  filter(!is.nan(share_r)) %>% 
+  mutate(Municipality_Name = gsub("TOWN OF |CITY OF |VILLAGE OF ", "", Municipality_Name))
+####
+
+wi <- wi %>% 
+  mutate(Municipality_Name = gsub("TOWN OF |CITY OF |VILLAGE OF ", "", trimws(gsub("-.*", "", Jurisdiction))),
+         County_Name = toupper(gsub(" County", "", County)),
+         ward = gsub(".*Ward ", "", Ward),
+         ward = ifelse(Municipality_Name == "TOWN OF LA GRANGE", gsub("A|B", "", ward), ward))
+
+wi2 <- left_join(wi, r16, by = c("County_Name", "Municipality_Name", "ward"))
 
 ####
 wi$cd <- as.integer(gsub("Congressional - District ", "", wi$Congressional))
